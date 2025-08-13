fba7df11f5079accc6ae8fd049159b67f00ee5c8
Willian <williansilvamamede@gmail.com>
2025-08-13 00:13:13 -0300

    lógica adicionada

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,22 +1,22 @@
 var prods = [
-    { id: 1, name: "Bife com batata", price: 30.0 },
-    { id: 2, name: "Coxa de frango Crocante", price: 25.0 },
-    { id: 3, name: "Carne de panela", price: 22.0 },
-    { id: 4, name: "Farofa", price: 10.0 },
-    { id: 5, name: "Salada", price: 8.0 },
-    { id: 6, name: "Torresmo", price: 12.0 },
+    { id: 1, name: "Skol 600", sku: 982 },
+    { id: 2, name: "Brahma 600", sku: 988 },
+    { id: 3, name: "Budweiser 600", sku: 2548 },
+    { id: 4, name: "Original 600", sku: 2546 },
+    { id: 5, name: "Spaten 600", sku: 23186 },
+    { id: 6, name: "Stella", sku: 20530 },
 ];
 
 
 
 
-function calc(){
+function calc() {
     var quantities = document.getElementsByName("quantity");
-    var output     = document.getElementById("output");
-    var total      = 0;
+    var output = document.getElementById("output");
+    var total = 0;
 
 
-    
+
     output.innerHTML = "";
 
     var formatter = new Intl.NumberFormat('pt-BR', {
@@ -24,22 +24,30 @@ function calc(){
         currency: 'BRL',
     });
 
-    output.innerHTML = `<p>Caro(a), ${document.getElementById("nome").value}</p><p>Seguem os dados do seu pedido.</p <p>Seu pedido é:</p>`;
+    output.innerHTML = `<p>Cliente: ${document.getElementById('nbCliente').value}</p>
+    <p>Revenda: ${document.querySelector('input[name=revendaRadio]:checked').value}</p>
+    <p>Motivo: ${document.getElementById('motivoGV').value} </p>
+    <p>Outro motivo: ${document.getElementById('outroMotivo').value} </p>
+    <p>Cofre: ${document.getElementById('cofreBoni').value} </p>
+    
+    <h4>Bonificar produtos</h4>`;
 
     for (var input of quantities) {
-        var id = input.id;        
-        
+        var id = input.id;
+
         if (input.value != 0) {
-            output.innerHTML += `Produto: ${prods[id-1].name}  - Preço: ${formatter.format(prods[id-1].price)} - Quantidade: ${input.value} </br>`;
-            total            += prods[id-1].price * parseFloat(input.value);
+            output.innerHTML += `Produto: ${prods[id - 1].name} 
+            | Sku: ${prods[id - 1].sku} 
+            | Quantidade: ${input.value} </br>`;
+            // total            += prods[id-1].sku * parseFloat(input.value);
         }
 
 
     }
 
-    output.innerHTML += `<h2>Total: ${formatter.format(total)}</h2>`;
+    // output.innerHTML += `<h2>Total: ${formatter.format(total)}</h2>`;
 
-    
+
 
 }
 
